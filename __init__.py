97f2e8d90ff7e56f3d5d1814f1e9b4d57dbc68f7
Ghost <rynjs0808@gmail.com>
2025-07-15 20:33:23 +0400

    Update : 1.6

--- __init__.py
+++ __init__.py
@@ -1,10 +1,37 @@
 # Download this file
 
 def imported() :
-    print("\n\nsetup V1.5 [public-version] GitHub repository\n")
+    print("\n\nsetup V1.6 [public-version] GitHub repository\n")
     print("Hello!\n")
 
 imported()
+
+def autoInstall_Pygame() :
+    import platform
+
+    system = platform.system()
+    if system == "Windows" :
+        print("Installing pygame")
+        import subprocess
+        
+        pygameInstall = "pip install pygame"
+        subprocess.Popen(pygameInstall, shell = True)
+    else : 
+        print(f"Please install pygame, auto-install doesn't work with {system}")
+
+def autoInstall_colorama() :
+    import platform
+
+    system = platform.system()
+    if system == "Windows" :
+        print("Installing Colorama")
+        import subprocess
+        
+        coloramaInstall = "pip install colorama"
+        subprocess.Popen(coloramaInstall, shell = True)
+    else :
+        print(f"Please install colorama, auto-install doesn't work with {system")
+
 
 class ask :
     def MissingPygame(self) :
@@ -12,34 +39,13 @@ class ask :
             import pygame
             print("Pygame installed")
         except : 
-            import platform
-
-            system = platform.system()
-            if system == "Windows" :
-                print("Installing pygame")
-                import subprocess
-                
-                pygameInstall = "pip install pygame"
-                subprocess.Popen(pygameInstall, shell = True)
-            else : 
-                print(f"Please install pygame, auto-install doesn't work with {system}")
+            autoInstall_pygame()
     def MissingColorama(self) :
         try :
             import colorama
             print("Colorama installed")
         except :
-            import platform
-
-            system = platform.system()
-            if system == "Windows" :
-                print("Installing Colorama")
-                import subprocess
-                
-                coloramaInstall = "pip install colorama"
-                subprocess.Popen(coloramaInstall, shell = True)
-            else :
-                print(f"Please install colorama, auto-install doesn't work with {system")
-
+            autoInstall_colorama()
 
 class Color :
     def White(self) :
